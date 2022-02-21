94a578cf5c810011af74cac302d37f0fa26cf831
MWTJC <mwtjc@outlook.com>
2022-02-21 08:56:27 +0800

    预备linux部署化

--- ocrtest.py
+++ ocrtest.py
@@ -4,6 +4,11 @@ import cv2
 from pprint import pprint
 
 if __name__ == '__main__':
-    img = cv2.imread('region.jpg')
-    res = MAIN_PROSS.net_OCR(img)
-    pprint(res.json()["results"][0]["data"])
+    img = cv2.imread('OCR_IMG\\Input_IMG\\zs-blood3.jpg')
+    pre_response = MAIN_PROSS.net_OCR(img)
+    if pre_response is 'OCROFFLINE':
+        print ('错误：OCR离线')
+    elif len(pre_response.json()["results"]) == 0:
+        pprint (f"错误：OCR没有正常工作,{pre_response.text}")
+    else:
+        pprint(pre_response.json()["results"][0]["data"])
